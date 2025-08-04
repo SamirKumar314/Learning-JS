fb1a62cfbe2299180d7a326f450223d9a0195ec0
SamirKumar314 <145570636+SamirKumar314@users.noreply.github.com>
2025-08-04 14:56:34 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,15 +1,23 @@
 "use strict"
 
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("samir")
+
 let name = "samir"
 let age = 18
 let isLoggedIn = false  //boolean
+let state; //undefined
 
 // number => 2 to power 53
 // bigint
-// string => " "
+// string => " " or ' '
 // boolean => true/false
 // null => standalone value and datatypes
-//undefined => value hasnt been assigned
+// undefined => value hasnt been assigned
 // symbol => unique
 
 //object
