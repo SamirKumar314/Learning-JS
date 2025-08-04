26510536c0728b795345cfe544b81a66d5fef963
SamirKumar314 <145570636+SamirKumar314@users.noreply.github.com>
2025-08-04 15:48:36 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,6 +18,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //Converting other datatypes into string
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+// *********************Operations*************************
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " samir"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2 //not ideal
+
+let gameCounter = 100;
+console.log(gameCounter++); //suffix increment
+
+console.log(gameCounter); //increases after the operation
+
+console.log(++gameCounter); //preffix increments first
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
