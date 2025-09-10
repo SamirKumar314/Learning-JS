145c02ef8c04d78a8bfa1efdd2aff4e256b09240
SamirKumar314 <samirkumar.0301@gmail.com>
2025-09-10 20:33:10 +0530

    oops in js

---
+++ 10_classes_and_oop/00_notes.md
@@ -0,0 +1,27 @@
+# JavaScript and classes
+Classes were introduced to javascript for creating familarity for C++ and java developers -
+- classes in javascript, fundamentally still works on prototypes
+
+## OOP - Object oriented programming
+- JS is a prototype-based programming language
+
+## Objects
+- collection of properties and methods.
+- toLowerCase, promise, etc. (e.g. objects of js)
+
+## Why use OOPs
+- brings few features from object oriented programming
+- better management of code chunk.
+
+## Parts of OOP
+- Object literals (strictly objects)
+- constructors function
+- prototypes
+- classes
+- instances(new and this keywords)
+
+## 4 important pillars of OOPs
+- Data Abstraction
+- Encapsulation
+- Inheritance
+- Polymorphism
---
+++ 10_classes_and_oop/01_oop.js
@@ -0,0 +1,47 @@
+//object literals---------
+const user = {
+    username: "samir",
+    loginCount: 8,
+    isLoggedIn: true,
+
+    getUserDetails: function(){
+        console.log("got users details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this);   //current context
+        
+    }
+}
+// console.log(user.username)
+// console.log(user.getUserDetails());
+// console.log(this);    //current context of global = {}
+
+
+//new keyword-----------------
+//creates new instances of a function
+function User(username, loginCount, isLoggedIn){
+    this.username = username          //this.username is the variable and username is the value passed to the function
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    //creating method inside the function also helps creates data abstraction
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+    return this
+}
+
+const userOne = new User("samir", 12, true)
+const userTwo = new User("chaiAurCode", 10, false)
+//if we do not use 'new' keyword, it wont create a new instance,
+//rather it overwrites the old values with the new values
+console.log(userOne);
+console.log(userTwo)
+console.log(userOne.constructor);   //shows reference to the function
+
+
+//*notes:-
+// - new keyword creates an empty object called instance.
+// - after creating instance, it calls a constructor functions(include all arguments)
+// - all the arguments gets injected to 'this' keyword.
+// - returns the object of the instance.
